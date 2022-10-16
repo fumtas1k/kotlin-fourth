44f245e7638bf79e034371cc8d7a8e942d801b97
fumtas1k <fumta01a7@gmail.com>
2022-10-16 18:15:15 +0900

    init

---
+++ src/main/kotlin/com/fumta/fourth/FourthApplication.kt
@@ -0,0 +1,11 @@
+package com.fumta.fourth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FourthApplication
+
+fun main(args: Array<String>) {
+	runApplication<FourthApplication>(*args)
+}
---
+++ src/test/kotlin/com/fumta/fourth/FourthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fumta.fourth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FourthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
